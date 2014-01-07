d1870cda29ec97c7134b2478453082363f8ac311
Casey Mullineaux <casey@nurfballs.com.au>
2014-01-07 11:04:06 +1100

    Fleshed out the gathering of IP info

--- Networking/Get-NetworkInformation.ps1
+++ Networking/Get-NetworkInformation.ps1
@@ -1,3 +1,55 @@
-﻿# Get Network Information
-Get-WMIObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE
+﻿function Export-NetworkInfo 
+{
+    param(
+        #[parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$True)]
+        [parameter(Mandatory=$True)]
+        [string[]]$ComputerName = $env:computername
+    )
+
+    begin{ New-Item -ItemType Directory -Force -Path C:\Hotline | Out-Null }
+    process {
+        ForEach ($Computer in $ComputerName) {
+            Write-Output "+ $Computer"
+            If (Test-Connection -ComputerName $ComputerName -Count 1 -ea 0) {
+                # Get Network Information
+                $Networks = Get-WMIObject -Class Win32_NetworkAdapterConfiguration -ComputerName $Computer -Filter IPEnabled=TRUE
+                ForEach ($Network in $Networks) {
+                    $IPAddress = $Network.IPAddress[0]
+                    $SubnetMask = $Network.IPSubnet[0]
+                    [String]$DefaultGateway =  $Network.DefaultIPGateway
+                    [String]$DNSServers = $Network.DNSServerSearchOrder
+                    $IsDHCPEnabled = $False
+                    If ($Network.DHCPEnabled) { $IsDHCPEnabled = $True }
+                    $MACAddress = $Network.MACAddress
+        
+                    $objOutput = New-Object PSObject
+                    $objOutput | Add-Member -MemberType NoteProperty -Name IPAddress -Value $IPAddress
+                    $objOutput | Add-Member -MemberType NoteProperty -Name SubnetMask -Value $SubnetMask
+                    $objOutput | Add-Member -MemberType NoteProperty -Name DefaultGateway -Value $DefaultGateway
+                    $objOutput | Add-Member -MemberType NoteProperty -Name IsDHCPEnabled -Value $IsDHCPEnabled
+                    $objOutput | Add-Member -MemberType NoteProperty -Name DNSServers -Value $DNSServers
+                    $objOutput | Add-Member -MemberType NoteProperty -Name MACAddress -Value $MACAddress
+                    $objOutput | Export-Csv -Path C:\Hotline\$Computer.csv -Append -NoTypeInformation
+                    $objOutput
+                }
+            }
+        }
+    }
+    end {}
+}
+
+
+function Import-NetworkInfo 
+{
+    param(
+    [parameter(Mandatory=$True)]
+    [String]$File
+    )
+
+
+}
+
+
+
+
 
