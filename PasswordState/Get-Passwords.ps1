7d45a31cf75024cc059d90219c87894c108e3c6a
Nurfballs <casey@nurfballs.com.au>
2016-06-21 16:41:27 +1000

    Delete Get-Passwords.ps1

--- PasswordState/Get-Passwords.ps1
+++
@@ -1,5 +0,0 @@
-﻿# Retrieve all passwords
-$results = Invoke-Restmethod -Uri "https://vault.hotlineit.com/api/passwords/?apikey=8d9e4f3c76b2a55c4b98673aefd2753f&queryall&format=json"
-
-# Display ADA NSW Passwords
-$results | Where-Object { $_.TreePath -like "*ADA NSW*" } | Select TreePath, PasswordList, Title, Description, Username, Password
